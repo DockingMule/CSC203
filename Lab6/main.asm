cddbfcbcb4cd61e4e551d818575184b8608e74c0
DockingMule <aca0824@gmail.com>
2019-05-01 21:58:21 -0400

    Update main.asm

--- Lab6/main.asm
+++ Lab6/main.asm
@@ -1,3 +1,10 @@
+; Austin Alloway
+; 5/1//2019
+; Lab 4
+; Purpose:
+;		user input starting and ending number
+;		and program generates random numbers as many
+;		times as user chooses.
 TITLE MASM Template						(main.asm)
 
 ; Import the Irvine library functions
@@ -5,82 +12,133 @@ INCLUDE Irvine32.inc
 
 ; Data declarations section used to declare variables for the program
 .data
+	startingNumberPrompt BYTE "What is the starting number? ",0
+	endingNumberPrompt BYTE "What is the ending number? ",0
+	numberOfGenerationsPrompt BYTE "How many times do you want to randomly generate a number? ",0
+	winPrompt BYTE "Congratulations! Your lucky number was a winner! It came out ",0
+	losePrompt BYTE "Sorry your number did not come out. You lose.", 0
+	luckyNumberPrompt BYTE "What is your lucky number? ", 0
+	winPromptEnd BYTE " time(s).", 0
+	fancyLines BYTE 30 DUP ('~-'),0
+	numberOfGenerations DWORD 0
+	startingNumber DWORD 0
+	endingNumber DWORD 0
+	luckyNumber DWORD 0
+	winCount DWORD 0
 
-;------------------------------------------------------------------------------
-; data variables
-;------------------------------------------------------------------------------
-	valueNumberPrompt BYTE "How many values are in the message? ", 0
-	codedMessagePrompt BYTE "Enter the values of the coded message: ", 0
-	decodedMessagePrompt BYTE "The decoded message: ", 0
-
-	codedMessage DWORD 25 DUP (0)
-	decodedMessage BYTE 25 DUP (0)
-	keypass BYTE ' ','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z', 0
-	valueCount DWORD 0
 
 ; Code section used to write the code for the program.
 .code
 main PROC
-	call Clrscr
-;------------------------------------------------------------------------------
-; initial Prompts
-;------------------------------------------------------------------------------
-	mov	 edx,OFFSET valueNumberPrompt
-	call WriteString
-	call ReadDec
-	mov valueCount, eax
 
-	MOV edx, OFFSET codedMessagePrompt
-	CALL WriteString
+	; ---------------------------------------------------
+	; Gathering starting, ending, and generation numbers
+	; ---------------------------------------------------
+ 
+    MOV  eax, yellow + (gray * 16)	
+    CALL   SetTextColor
+
+	call Clrscr
+
+	MOV edx, OFFSET fancyLines	
+	CALL writeString
 
 	CALL crlf
 
-;------------------------------------------------------------------------------
-; loop for codedmessage string
-;------------------------------------------------------------------------------
-	mov ecx, valueCount
-	mov ebx, 0
-	codedMessageLoop:
-		CALL ReadDec
-		mov [codedmessage + ebx * 4], eax
-		INC ebx
-		Loop codedMessageLoop
-;------------------------------------------------------------------------------
-; User Proceduer call
-;------------------------------------------------------------------------------
-	CALL key
+	MOV edx, OFFSET startingNumberPrompt		; Getting starting number
+	CALL writeString
+	CALL readDec
+	MOV startingNumber, eax
 
-;------------------------------------------------------------------------------
-; Final outputs
-;------------------------------------------------------------------------------
-	mov edx, OFFSET decodedMessagePrompt
+	MOV edx, OFFSET endingNumberPrompt			; Getting ending number
+	CALL writeString
+	CALL readDec
+	MOV endingNumber, eax
+
+	MOV edx, OFFSET numberOfGenerationsPrompt	; Getting number of generations	
+	CALL writeString
+	CALL readDec
+	MOV numberofGenerations, eax
+
+	MOV edx, OFFSET luckyNumberPrompt	; Getting number of generations	
+	CALL writeString
+	CALL readDec
+	MOV luckyNumber, eax
+
+	MOV edx, OFFSET fancyLines	
+	CALL writeString
+
+	CALL crlf
+
+	
+	; ---------------------------------------------------
+	; Looping each generation and printing random number
+	; ---------------------------------------------------
+
+	MOV edx, startingNumber			; for myRandom starting number
+	MOV ebx, endingNumber			; for myRandom ending number
+	MOV ecx, numberOfGenerations	; for loop iterations
+	CALL Randomize
+
+	L1:
+		CALL myRandom			; user defined procedure
+		CALL writeDec
+		CMP eax, luckyNumber
+		JNE Next
+		inc winCount		; if the luckyNumber is pulled increment win number by one
+		Next:				; if it is not pulled do not increment
+			CALL crlf
+			LOOP L1
+
+	; ---------------------------------------------------
+	; final prompt
+	; ---------------------------------------------------
+
+
+	MOV edx, OFFSET fancyLines		
+	CALL writeString
+
+	CALL crlf
+
+	cmp winCount, 0				
+	JE LOSE						
+	MOV edx, OFFSET winPrompt		; if the number of times luckyNumber is pulled is greater than one
+	CALL writeString				; print winPrompt
+	MOV eax, winCount
+	CALL WriteDec
+	MOV edx, OFFSET winPromptEnd
 	CALL WriteString
+	JMP WIN
+	LOSE:
+		MOV edx, OFFSET losePrompt	; else print losePrompt
+		CALL writeString
+	WIN:
+		CALL crlf
 
-	mov edx, OFFSET decodedMessage
-	CALL WriteString
-
-	exit
+		CALL crlf
+	
+		exit
 main ENDP
 
+
 ;----------------------------------------------------
-key PROC
-; purpose: 
+myRandom PROC
+; purpose: random number within staring and ending number
 ; Recieves:
+;	edx - starting number
+;	ebx - ending number
+;
 ; Returns:
+;	eax - contains random number
 ;----------------------------------------------------
-mov eax, 0
-mov edx, 0
-mov ebx, 0
-mov ecx, valueCount
 
-decoderLoop:
-	mov eax, [codedMessage + ebx * 4]
-	mov dl, [keypass + eax]
-	mov [decodedMessage + ebx], dl
-	INC ebx
-	LOOP decoderLoop
-
+	MOV eax, ebx
+	SUB eax, edx
+	INC eax
+	CALL RandomRange
+	ADD eax, edx
 	ret
-key ENDP		;end of user defined procedure
+
+myRandom ENDP		;end of user defined procedure
 
 END main
